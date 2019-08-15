3805db4d81ab736386167a788a934b920c2e3397
udonka <icymasa@gmail.com>
2019-08-15 10:21:52 +0900

    eject してjsonが表示できるようにはなった。

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React,{Component} from 'react';
+import fetch from 'node-fetch';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import JsonPress from './JsonPress.js';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      url: "/",
+      json: []
+    }
+
+  }
+
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+
+  }
+
+
+  render(){
+    return  pug`
+      .c-top-bar 
+        .e-inner
+          .e-title JsonPress
+      .c-main-column.c-page-column
+        Router
+          Route(exact path="/" render=${(props)=>pug`p home`})
+          Route(path="/edit/:id.json" render=${(props)=>{
+            console.log("props.match.params.id")
+            console.log(props.match.params.id)
+
+            return pug`
+              JsonPress(url="/"+props.match.params.id + ".json")
+            `;
+          }})
+
+
+    `;
+  }
 }
 
 export default App;
---
+++ src/JsonPress.js
@@ -0,0 +1,132 @@
+import React,{Component} from 'react';
+import './App.scss';
+
+function isBlock(value){
+  if(typeof value == "object"){ //オブジェクトや配列はブロック
+    return true;
+  }
+  else if(typeof value == "string" && value.length > 100){ //長いテキストもブロック
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+export default class JsonPress extends Component{
+  constructor(props){
+    super(props);
+
+
+    this.state = {
+      json:[]
+    }
+    console.log("props.url");
+    console.log(props.url);
+    fetch(props.url)
+      .then((res)=> res.text())
+      .then((text)=>{
+        console.log("text");
+        console.log(text);
+        try{
+          const parsedJson = JSON.parse(text);
+
+          this.setState({
+            json: parsedJson
+          });
+
+        }
+        catch(e){
+          console.log(props.url +"のjsonがパースできませんでした。");
+        }
+      });
+  }
+
+
+  ValuePress(value){
+    return pug`
+      if Array.isArray(value)
+        = this.ArrayPress(value)
+      else if typeof value == "object"
+        = this.ObjectPress(value)
+      else if typeof value == "string"
+        = this.StringPress(value)
+      else if typeof value == "number"
+        = this.NumberPress(value)
+      else if typeof value == "boolean"
+        = this.BooleanPress(value)
+      //
+        else
+          p(style={background:"red"})= value
+    `;
+  }
+
+  //// refarence types ////
+  ObjectPress(object){
+    return pug`
+      .e-value.m-object
+        - const keys = Object.keys(object)
+        each key in keys
+          .e-pair(key=key className=isBlock(object[key]) ? "m-block":"")
+            .e-key
+              = this.ValuePress(key)
+            .e-cont
+              = this.ValuePress(object[key])
+    `;
+  }
+
+  ArrayPress(array){
+
+    return pug`
+      .e-value.m-array
+        each item,index in array
+          .e-cont(key=index className=isBlock(item) ? "m-block":"")
+            =this.ValuePress(item)
+
+    `;
+  }
+
+
+
+    //// value types ////
+  StringPress(string){
+    return pug`
+      .e-value.m-string= string
+    `;
+  }
+
+  NumberPress(number){
+    return pug`
+      .e-value.m-number= number
+    `;
+  }
+
+
+  BooleanPress(bool){
+    console.log("bool");
+    console.log(bool);
+    return pug`
+      .e-value.m-boolean= bool ? "true":"false"
+    `
+  }
+
+  JsonPress(json){
+
+
+    return pug`
+      .c-json-press
+        if Array.isArray(json )
+          = this.ArrayPress(json)
+        else if typeof json == "object"
+          = this.ObjectPress(json)
+
+    `;
+  }
+
+  render(){
+    return  pug`
+      =this.JsonPress(this.state.json)
+    `;
+  }
+}
+
